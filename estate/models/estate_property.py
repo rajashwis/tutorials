3980bdf1f493bc871cf8dc4400c1d62c96e24ec2
rajashwis <rajashwis@gmail.com>
2024-07-08 12:05:01 +0545

    Added buyers and salespersons to the Estate Property Model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -31,3 +31,6 @@ class EstateProperty(models.Model):
     active = fields.Boolean(default = True)
 
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+
+    buyer_id = fields.Many2one('res.partner', string="Buyer")
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', copy = False, default=lambda self: self.env.uid)
