b34b2059e021567aac9bf5c31d5ce6e9122a6ebd
rajashwis <rajashwis@gmail.com>
2024-07-09 12:35:14 +0545

    Used t-out and markup to display html

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { markup, Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
 
@@ -8,4 +8,10 @@ export class Playground extends Component {
     static template = "awesome_owl.playground";
 
     static components = { Counter, Card };
+
+    setup() {
+        this.content1 = "<div class='text-primary'>some content</div>";
+        this.content2 = markup("<div class='text-primary'>some content</div>");
+    }
+
 }
